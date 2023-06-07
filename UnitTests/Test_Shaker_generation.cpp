fcd2446f6a4779a61c92375eb5f82d21d880a60f
Thomas GUILLEMIN <thomas.guillemin@conduent.com>
2023-06-07 18:39:26 +0200

    Add empty test file for shaker generation

---
+++ UnitTests/Test_Shaker_generation.cpp
@@ -0,0 +1,20 @@
+
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+#endif
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+#include "DiskContainer.h"
+
+#include "TestUtils.h"
+
+TEST(Shaker_generation, Test_1)
+{
+   TestDump test_dump;
+   CommandList cmd_list;
+
+}
+
