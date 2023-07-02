cea42b605bfe185d8f1dd05416ad2f2fc0ab9652
Thomas Guillemin <thomas.guillemin@gmail.com>
2023-07-02 22:27:43 +0200

    VSync on gate array

--- AmstradCore/GateArray.cpp
+++ AmstradCore/GateArray.cpp
@@ -50,6 +50,7 @@ void GateArray::Sequencer()
 void GateArray::SequencerDecodeDown()
 {
    u313_ = (~u312_) & 0x1;
+   // CASAD - todo
 }
 
 void GateArray::SequencerDecode()
@@ -58,16 +59,196 @@ void GateArray::SequencerDecode()
    line_4_mhz_->ForceLevel((~ (((s_ >> 1) ^ (s_ >> 3)) | ( (s_>>5)^(s_>>7)))) & 0x1);
    u312_ = (((s_ >> 6) | (~(s_ >> 2))) & s_) & 0x1;
 
+   // RAS - todo
 
-   // RAS
-   // CASAD
+
+}
+
+// TODO : find the best place to call this (RAM access ?)
+void GateArray::RomMapping()
+{
+   // Input : LROMEN, A15, A14, HROMEN, MREQ, RD
+   // Output : ROMEN, RAMRD
+   unsigned char u601 = ((lromen_?0:1) & (~(bus_address_->data_ >> 15)) & (~(bus_address_->data_ >> 14)) ) & 0x01;
+   unsigned char u602 = (bus_address_->data_ >> 15) & (bus_address_->data_ >> 14) & (hromen_ ? 0 : 1);
+   unsigned char u603 = (u601 | u602) & 0x1;
+   romen_ = ((~u603) | line_rd_->GetLevel() & line_mreq_->GetLevel())& 0x1;
+   ramrd_ = ((u603) | line_rd_->GetLevel() & line_mreq_->GetLevel()) & 0x1;
+}
+
+void GateArray::CasGeneration()
+{
+   // todo
+}
+
+void GateArray::InkRegister()
+{
+   // Input : INKSEL; DATA(0-4), INKRE
+   // output : INKR
+
+   // 
+   // TODO
+   // 
+}
+
+void GateArray::HSync(bool set)
+{
+   if (set)
+   {
+      // Counters
+      u815 = ~u815 & 0x1;
+      if (!u815)
+      {
+         u821 = ~u821 & 0x1;
+         if (!u821)
+         {
+            u826 = ~u826 & 0x1;
+            if (!u826)
+            {
+               u830 = ~u830 & 0x1;
+               if (!u830)
+               {
+                  u832 = ~u832 & 0x1;
+                  if (!u832)
+                  {
+                     u835 = ~u835 & 0x1;
+                     if (!u835)
+                     {
+                        // Clock u836
+                        u836 = 1;
+                        line_int_->ForceLevel(0);
+                        // todo : check int + m1 + iorq or reset
+                     }
+                  }
+               }
+            }
+
+            u816 = (u826 & u832 & u835 & 0x1) | u816;
+         }
+      }
+
+      // Reset U808-13-18-24
+      u808 = u813 = u818 = u824 = 0;
+      mode_sync = false;
+
+      // Clock U809
+      u809 = ~u809 & 0x1;
+      if (!u809)
+      {
+         u814 = ~u814 & 0x1;
+         if (!u814)
+         {
+            u820 = ~u820 & 0x1;
+            if (!u820)
+            {
+               u825 = ~u825 & 0x1;
+               if (!u825)
+               {
+                  u829 = ~u829 & 0x1;
+                  if (!u829)
+                  {
+
+                  }
+               }
+            }
+         }
+      }
+      nsync_ = !(false ^ (u820 & (~u825) & (~u829)));
+      
+      // Reset HCNT ?
+      if (u820 & u825 & u829 & 0x1) 
+      {
+         // reset
+         u809 = u814 = u820 = u825 = u829 = 0;
+         hcntlt28_ = false;
+      }
+      else
+      {
+         hcntlt28_ = true;
+      }
+
+      if (u820 & (~u825) & (~u829) & 0x1)
+      {
+         u817 = (~u812) & 0x1;
+      }
+      else
+      {
+         u817 = false;
+      }
+      if (u816 | u817)
+      {
+         u815 = u821 = u826 = u830 = u832 = u835 = 0;
+      }
+   }
+   else
+   {
+      u815 = u821 = u826 = u830 = u832 = u835 = 0;
+      u816 = 0;
+   }
+
+   
+}
+
+void GateArray::VSync(bool set)
+{
+   // Input : HSYNC, CCLK, RESET, VSYNC, IORQ, M1, IRQ_RESET
+   // Output : INT, NSYNC, MODE_SYNC, HCNTL28
+}
+
+void GateArray::RegisterDecode()
+{
+   // Only active if M1, A15/14 = 01, IORQ, S0S7 =11
+   if (((s_ & 0x81) == 0x81)
+      && (line_iorq_->GetLevel() == 0)
+      && ((bus_address_->data_ & 0xC000) == 0x4000)
+      && (line_m1_->GetLevel() == 1)
+      )
+   {
+      switch (bus_data_->data_ & 0xC0)
+      {
+      case 0x00:
+         inksel_ = bus_data_->data_ & 0x1F;
+         break;
+      case 0x80:
+         hromen_ = bus_data_->data_ & 0x8;
+         lromen_ = bus_data_->data_ & 0x4;
+         mode_ = bus_data_->data_ & 0x3;
+         break;
+      case 0x40:
+         if (inksel_ & 0x10)
+         {
+            // todo handle reset 
+            border_ = bus_data_->data_ & 0x1F;
+         }
+         inkre_ = true;
+         // Ink register
+         InkRegister();
+
+         break;
+      case 0xC0:
+         // Nothing !
+         break;
+      }
+   }
+   irq_reset_ = (((s_ & 0x81) == 0x81)
+      && (line_iorq_->GetLevel() == 0)
+      && ((bus_address_->data_ & 0xC000) == 0x4000)
+      && (line_m1_->GetLevel() == 1)
+      && ((bus_data_->data_ & 0xD0) == 0x50)
+      );
 
 }
 
 void GateArray::TickUp()
 {
+   ////////////////////////
+   // These sequences needs clock and s_ register
    SequencerDecode();
+   CasGeneration();
 
+
+   ////////////////////////
+   // Finalise : All that does immédiately need s_.
    Sequencer();
 
    // WAIT
@@ -80,10 +261,8 @@ void GateArray::TickUp()
    // CLK
    line_CCLK_mhz_->ForceLevel((~(S(2) | S(5)))&0x1);
 
-   // MWE
-   // 244E
-
-
+   // MWE - todo
+   // 244E - todo
 }
 
 void GateArray::TickDown()
--- AmstradCore/GateArray.h
+++ AmstradCore/GateArray.h
@@ -18,6 +18,9 @@ public:
    void TickUp() override;
    void TickDown() override;
 
+   void VSync(bool set);
+   void HSync(bool set);
+
    // Various lines
    DECLARE_REFERENCE_LINE(line_4_mhz_);
    DECLARE_REFERENCE_LINE(line_CCLK_mhz_);
@@ -33,6 +36,7 @@ public:
    DECLARE_REFERENCE_LINE(line_m1_);
    DECLARE_REFERENCE_LINE(line_iorq_);
    DECLARE_REFERENCE_LINE(line_rd_);
+   DECLARE_REFERENCE_LINE(line_mreq_);
 
    DECLARE_REFERENCE_BUS_ADDRESS(bus_address_);
    DECLARE_REFERENCE_BUS_DATA(bus_data_);
@@ -42,14 +46,33 @@ protected:
    void Sequencer();
    void SequencerDecode();
    void SequencerDecodeDown();
+   void RegisterDecode();
+   void InkRegister();
+   void RomMapping();
+   void CasGeneration();
 
    unsigned char s_;
    unsigned int counter;
 
    unsigned char u312_, u313_;
+   unsigned char u815, u821, u826, u830, u832, u835, u812, u808, u813, u818, u824;
+   unsigned char u809, u814, u820, u825, u829, u803, u836;
+   unsigned char u816, u817, u831, u806, u828;
 
+   unsigned char border_;
+   unsigned char mode_;
+   unsigned short inkr_[5];
+   unsigned char inkre_;
+   unsigned char inksel_;
+   bool irq_reset_;
+   bool hromen_;
+   bool lromen_;
 
-
+   bool romen_;
+   bool ramrd_;
+   bool mode_sync;
+   bool nsync_;
+   bool hcntlt28_;
 
 };
 
